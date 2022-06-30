47f8abcbde3c17002c8cc9f4fd398cc3e15f3f95
Tornelius <tkbjr28@icloud.com>
2022-06-30 10:07:07 -0400

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Tornelius Broadwater, Jr on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
